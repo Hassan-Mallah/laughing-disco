21319fd2361628c118b64280151505fb48bf4cd7
Hassan <hassansmallah@gmail.com>
2022-03-31 11:55:58 +0300

    3d array

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import numpy
 
-print('hello world')
+
+def create_3d_array(num):
+    return numpy.array(num)
+
+
+num = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]]]
+
+num3 = create_3d_array(num)
+print('standard array', num, '\nnumpy array\n', num3)
