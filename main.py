61ee7d59dc9a80b621dc36bf6daa663f06c8796f
Hassan <hassansmallah@gmail.com>
2022-04-03 15:15:10 +0300

    shape

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-# create an array
-a = np.arange(15).reshape(3, 5)
 
-print(a)
+# create an array
+def create_array(size):
+    return np.arange(size)
+
+
+arr = create_array(15)
+a = arr.reshape(3, 5)
+
+print('array:\n', a)
+print('shape:', a.shape)
