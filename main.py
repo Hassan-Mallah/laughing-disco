95da6d0943af48db6b2be7bd7aae3ca63e6b35f3
Hassan <hassansmallah@gmail.com>
2022-04-04 17:58:56 +0300

    array info

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ def create_array(size):
     return np.arange(size)
 
 
-arr = create_array(15)
-a = arr.reshape(3, 5)
+def array_info(arr):
+    print('array:\n', arr)
+    print('shape:',
+          arr.shape)  # array shape, This is a tuple of integers indicating the size of the array in each dimension
+    print("dimensions:", arr.ndim)  # the number of axes (dimensions) of the array.
+    print("size:",
+          arr.size, '\n')  # the total number of elements of the array. This is equal to the product of the elements of shape.
 
-print('array:\n', a)
-print('shape:', a.shape)
+
+my_arr = create_array(15)
+array_info(my_arr)
+
+my_arr = my_arr.reshape(3, 5)
+
+array_info(my_arr)
